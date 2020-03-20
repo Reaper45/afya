18a877964b609a0426a40a77a84461665c3cf6f6
Joram <jomwashighadi@gmail.com>
2020-03-21 02:13:13 +0300

    Theming & Router setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { theme } from "theme";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router-dom"
+import { ThemeProvider } from "emotion-theming";
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory()
+
+ReactDOM.render(
+  <Router history={history}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+export interface ITheme {
+  colors: {
+    primary: string;
+    dark: string;
+    light: string;
+    secondary: string;
+  };
+}
+
+export const theme: ITheme = {
+  colors: {
+    primary: "#805AD5",
+    dark: "#44337A",
+    light: "#FAF5FF",
+    secondary: "#222222"
+  }
+};

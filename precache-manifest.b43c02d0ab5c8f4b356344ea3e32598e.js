3faf6a81e548f55a0c9530769579b63f20c1bb24
Joram <jomwashighadi@gmail.com>
2020-03-31 13:08:58 +0300

    Updates

---
+++ precache-manifest.b43c02d0ab5c8f4b356344ea3e32598e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54a8f29a4a27b7bdb39b92ebea88fc9",
+    "url": "/afya/index.html"
+  },
+  {
+    "revision": "317172bef8c3339ceca7",
+    "url": "/afya/static/css/2.a7ba36b5.chunk.css"
+  },
+  {
+    "revision": "e7b1d6fd0f78ed5bebe3",
+    "url": "/afya/static/css/main.36597463.chunk.css"
+  },
+  {
+    "revision": "317172bef8c3339ceca7",
+    "url": "/afya/static/js/2.5224354c.chunk.js"
+  },
+  {
+    "revision": "0946f17034c4fe4046aaabc96447cd42",
+    "url": "/afya/static/js/2.5224354c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b1d6fd0f78ed5bebe3",
+    "url": "/afya/static/js/main.cc18e070.chunk.js"
+  },
+  {
+    "revision": "2ea3fdaf0d7a9f0d8ad0",
+    "url": "/afya/static/js/runtime-main.a2ecf579.js"
+  },
+  {
+    "revision": "72b63f3e05e8bccc6c07ad66095ab118",
+    "url": "/afya/static/media/avatar.72b63f3e.jpg"
+  }
+]);
